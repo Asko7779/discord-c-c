e3b4c1c4b1ee5f75d3b4e934bd82d18cca965916
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-03-24 22:07:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ import discord
 from discord.ext import commands
 
 TOKEN = "your bot token"
-
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
